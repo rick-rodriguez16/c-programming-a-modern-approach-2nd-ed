d4e636e5d15a5aff5603e8c782877804c67f6400
rick-rodriguez16 <rickrod6502@gmail.com>
2025-12-10 23:43:12 -0500

    ch ex1 done

--- ch3-formatted-input-output/ex1.c
+++ ch3-formatted-input-output/ex1.c
@@ -4,18 +4,24 @@
 
 #include <stdio.h>
 
+#define CORRECT_DATE_INPUT 3
+
 int main(void) {
 
     int month = 0;
     int day = 0;
     int year = 0;
+    
     printf("Enter a date (mm/dd/yyyy): ");
     fflush(stdout);
-    if (scanf("%d/%d/%d", &month, &day, &year) != 3) {
+    
+    if (scanf("%d/%d/%d", &month, &day, &year) == CORRECT_DATE_INPUT) {
+        printf("You entered the date %d%2.2d%2.2d\n", year, month, day);
+    }
+    else {
         printf("Error. Invalid input.\n");
     }
 
-    printf("You entered the date %d%2.2d%2.2d\n", year, month, day);
     return 0;
 }
 
