987c31366ec9be395a39db17e1fe8cd6cfad78fd
rick-rodriguez16 <rickrod6502@gmail.com>
2025-12-20 23:41:55 -0500

    ch3 ex4 completed

---
+++ ch3-formatted-input-output/ex4.c
@@ -0,0 +1,46 @@
+// Phone number converter
+// from (xxx) xxx-xxxx to xxx.xxx.xxxx
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PARTS_OF_PHONE_NUMBER 3
+
+int main(void) {
+    
+    puts("\tUS Phone number converter");
+    puts("Converts from (xxx) xxx-xxxx to xxx.xxx.xxxx");
+    puts("============================================\n");
+    
+    int area_code = -1;
+    int telephone_prefix = -1;
+    int line_number = -1;
+
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    fflush(stdout);
+    int phone_number_parts = scanf("(%d) %d-%d", &area_code, &telephone_prefix, &line_number);
+
+    if (phone_number_parts != PARTS_OF_PHONE_NUMBER) {
+        puts("Error. Incorrect number of phone number parts.");
+        return EXIT_FAILURE;
+    }
+
+    if (area_code < 200 || area_code > 999) {
+        puts("Error. Invalid area code.");
+        return EXIT_FAILURE;
+    }
+
+    if (telephone_prefix < 200 || telephone_prefix > 999) {
+        puts("Error. Invalid telephone prefix.");
+        return EXIT_FAILURE;
+    }
+    
+    if (line_number < 0 || line_number > 9999) {
+        puts("Error. Invalid line number.");
+        return EXIT_FAILURE;
+    }
+
+    printf("You entered: %d.%d.%04d\n", area_code, telephone_prefix, line_number);
+    return 0;
+}
+
